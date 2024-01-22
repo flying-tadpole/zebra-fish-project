797d3011ba713c615f715ec486f9f6c3677941af
Archer Whitesell Curry <awc1051@orad.northwestern.edu>
2024-01-22 15:54:23 -0600

    Added typeDefs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,41 @@
+const { Fish, Lab } = require("../models")
+
 const resolvers = {
     Query: {
-
+        //get all labs
+        labs: async () => {
+            return Lab.find()
+        },
+        //get all fish
+        fish: async () => {
+            return Fish.find()
+        }
     },
     Mutation: {
-
+        //add a lab
+        addLab: async (
+            parent, 
+            { pi, protocols }
+            ) => {
+                const lab = await Lab.create({
+                    pi,
+                    protocols
+                });
+                return lab
+        },
+        addFish: async (
+            parent,
+            { cageCard, lab, strain, location, dateOfHatch }
+            ) => {
+                const fish = await Fish.create({
+                    cageCard,
+                    lab,
+                    strain,
+                    location,
+                    dateOfHatch
+                });
+                return fish
+        }
     }
 }
 
