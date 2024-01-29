118b011a1f12f420c69c1ba10e14bee447be67f2
Archer Whitesell Curry <awc1051@orad.northwestern.edu>
2024-01-29 15:24:02 -0600

    front and back end connected, server working

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,11 +5,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5000,
+    port: 80,
     open: true,
     proxy: {
       "/graphql": {
-        target: "http://localhost:5000",
+        target: "http://localhost:80",
         changeOrigin: true,
         secure: false,
       },
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
