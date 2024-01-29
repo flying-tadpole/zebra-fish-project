5ee28a574d257d33ffdba22cb6efe442b4feca3a
Archer Whitesell Curry <awc1051@orad.northwestern.edu>
2024-01-29 14:36:40 -0600

    adding in front end app structure

---
+++ client/src/components/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <main>
+            <div>
+                <h2>ZebraFish??</h2>
+            </div>
+        </main>
+    )
+}
+
+export default Home
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from './App.jsx'
+import ErrorPage from './components/pages/ErrorPage.jsx';
+import Home from './components/pages/HomePage.jsx';
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 )
